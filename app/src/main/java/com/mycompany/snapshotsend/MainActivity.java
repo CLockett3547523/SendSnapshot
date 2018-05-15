25c5193eaf30c4b1b947eaf26b7696869fa703bd
Cameron Lockett <s3547523@student.rmit.edu.au>
2018-05-15 13:56:14 +1000

    Still needs testing. UI for snapshot activity has been finished? with simnple button added to Main to get to snapshot

--- app/src/main/java/com/mycompany/snapshotsend/MainActivity.java
+++ app/src/main/java/com/mycompany/snapshotsend/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mycompany.snapshotsend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SnapshotClick(View view) {// function named in OnClick
+        startActivity(new Intent(MainActivity.this, SnapshotSendActivity.class));
+        // come back via onResume() event.
     }
 }
